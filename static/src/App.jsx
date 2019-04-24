d6eff8d62aa5f3844a96507723b5d2753377cefa
Unknown <unknown@example.com>
2019-04-24 21:51:37 +0800

    图书馆关门了 --author:MRCUIXIANGJIE

--- static/src/App.jsx
+++ static/src/App.jsx
@@ -1,6 +1,10 @@
 const contentNode = document.getElementById('contents');
-const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
-const component = <p>{message}</p>;
-ReactDOM.render(component,contentNode);
+class IssueList extends React.Component{
+	render(){
+		return (
+			<div>This is a placeholder for the issue list.</div>
+		);
+	}
+}
+ReactDOM.render(<IssueList />,contentNode);
 
