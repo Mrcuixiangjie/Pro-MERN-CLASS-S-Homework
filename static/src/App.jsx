f5f5e855f2a894c615cefba4dc7992f88705930d
Unknown <unknown@example.com>
2019-04-25 10:44:42 +0800

    3.3　Issue Tracker组件组装原来在React里类还可以这么玩．
    --author:MRCUIXIANGJIE

--- static/src/App.jsx
+++ static/src/App.jsx
@@ -1,8 +1,36 @@
 const contentNode = document.getElementById('contents');
+class IssueFilter extends React.Component{
+	render(){
+		return(
+		   <div>This is a placeholder for the Issue Filter.</div>
+		)
+	}
+}
+class IssueTable extends React.Component{
+	render(){
+		return(
+			<div>This is a placeholder for a table of Issues.</div>
+		)
+	}
+}
+class IssueAdd extends React.Component{
+	render(){
+		return(
+			<div>This is a placeholder for an Issue Add entry form.</div>
+		)
+	}
+}
 class IssueList extends React.Component{
 	render(){
-		return (
-			<div>This is a placeholder for the issue list.</div>
+		return(
+			<div>
+				<h1>Issue Tracker</h1>
+				<IssueFilter />
+				<hr />
+				<IssueTable />
+				<hr />
+				<IssueAdd />
+			</div>
 		);
 	}
 }
