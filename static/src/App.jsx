ad938fa809f5ebd7294aef1204b12f075fed22e7
Unknown <unknown@example.com>
2019-05-06 18:40:16 +0800

    4.5无状态组件
    @author:MRCUIXIANGJIE

--- static/src/App.jsx
+++ static/src/App.jsx
@@ -11,52 +11,43 @@ const issues = [
 		completionDate:new Date('2019-5-01'),
 		title:'Missing bottom border on panel',
 	},
-];
-class IssueRow extends React.Component{
-	render(){
-		const issue=this.props.issue;
-		return(
-			<tr>
-				<td>{issue.id}</td>
-				<td>{issue.status}</td>
-				<td>{issue.owener}</td>
-                                <td>{issue.created.toDateString()}</td>
- 				<td>{issue.effort}</td>
-                                <td>{issue.completionDate ?
-				 issue.completionDate.toDateString() : ''}</td>
-				<td>{issue.title}</td>
-			</tr>
-		)
-	}
+]
+const IssueRow = (props) =>(
+        <tr>
+                <td>{props.issue.id}</td>
+                <td>{props.issue.status}</td>
+                <td>{props.issue.owner}</td>
+                <td>{props.issue.created.toDateString() }</td>
+                <td>{props.issue.effort}</td>
+                <td>{props.issue.completionDate ?
+                props.issue.completionDate.toDateString() : ''}</td>
+                <td>{props.issue.title}</td>
+        </tr>
+)
+function IssueTable(props){
+        const issueRows = props.issues.map(issue =><IssueRow
+        key={issue.id} issue={issue} />);
+        return(
+                <table className="bordered-table">
+                        <thead>
+                                <tr>
+                                        <th>Id</th>
+                                        <th>Status</th>
+                                        <th>Owner</th>
+                                        <th>Created</th>
+                                        <th>Effort</th>
+                                        <th>Completion Date</th>
+                                        <th>Title</th>
+                                </tr>
+                        </thead>
+                        <tbody>{issueRows}</tbody>
+                </table>
+        );
 }
 class IssueFilter extends React.Component{
 	render(){
 		return(
 		   <div>This is a placeholder for the Issue Filter.</div>
-		)
-	}
-}
-class IssueTable extends React.Component{
-	render(){
-		const issueRows = this.props.issues.map(issue => <IssueRow
-		key={issue.id} issue={issue} />)
-		return(
-			<table className="bordered-table">
-				<thead>
-					<tr>
-						<th>Id</th>
-						<th>Status</th>
-						<th>Owener</th>
-                                                <th>Created</th>
-						<th>Effort</th>
-                                                <th>Completion</th>
-						<th>Title</th>
-					</tr>
-				</thead>
-				<tbody>
-					{issueRows}
-				</tbody>
-			</table>
 		)
 	}
 }
@@ -122,4 +113,5 @@ class IssueList extends React.Component{
 	}
 }
 ReactDOM.render(<IssueList />,contentNode);
+
 
--- static/static/App.js
+++
@@ -1,9 +0,0 @@
-const contentNode = document.getElementById('contents');
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
-const component = React.createElement(
-  'p',
-  null,
-  message
-);
-ReactDOM.render(component, contentNode);
