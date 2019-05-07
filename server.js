ec21cf6855eed9d337dda71bb00398bc325beae7
Unknown <unknown@example.com>
2019-05-07 11:26:54 +0800

    5.7错误处理
    @author:MRCUIXIANGJIE

--- server.js
+++ server.js
@@ -3,6 +3,49 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(express.static('static'));
 app.use(bodyParser.json());
+const validIssueStatus = {
+	New: true,
+	Open: true,
+	Assigned: true,
+	Fixed: true,
+	Verified: true,
+	Closed: true,
+};
+const issueFieldType = {
+	id: 'required',
+	status: 'required',
+	owner: 'required',
+	effort: 'optional',
+	created: 'required',
+	completionDate: 'optional',
+	title: 'required',
+};
+function validateIssue(issue){
+	for(const field in issueFieldType){
+		const type= issueFieldType[field];
+		if(!type){
+			delete issue[field];
+		}else if(type=== 'required' && !issue[field]){
+			return `${field} id required.`;
+		}
+	}
+	if(!validIssueStatus[issue.status])
+		return `${issue.status} is not a valid status.`;
+	return null;
+}
+app.post('/api/issues',(req,res) =>{
+	const newIssue =req.body;
+	newIssue.id=issues.length+1;
+	newIssue.created=new Date();
+	if(!newIssue.status)
+		newIssue.status = 'New';
+	const err = validateIssue(newIssue)
+	if(err){
+		res.status(422).json({message: `Invalid requrest: ${err}` });
+		return;
+	}
+	issues.push(newIssue);
+});
 const issues = [
 	{
 　      id: 1,status: 'Open',owener: 'Ravan',
@@ -16,15 +59,6 @@ const issues = [
 	title: 'Missing bottom border on panel',
 	},
 ];
-app.post('/api/issues',(req,res) =>{
-	const newIssue = req.body;
-	newIssue.id = issues.length+1;
-	newIssue.created = new Date();
-	if(!newIssue.status)
-		newIssue.status = 'New';
-	issues.push(newIssue);
-	res.json(newIssue);
-});
 app.get('/api/issues',(req,res) =>{
 	const metadata = {total_count: issues.length };
 	res.json({ _metadata: metadata, records: issues });
