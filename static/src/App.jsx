d3f996082ae28aa39b88357bb3963dec0dc68948
Unknown <unknown@example.com>
2019-05-22 15:35:39 +0800

    6.4從MongoDB讀取數據 @author:MRCUIXIANGJIE

--- static/src/App.jsx
+++ static/src/App.jsx
@@ -1,7 +1,7 @@
 const contentNode = document.getElementById('contents');
 const IssueRow = (props) =>(
         <tr>
-                <td>{props.issue.id}</td>
+                <td>{props.issue._id}</td>
                 <td>{props.issue.status}</td>
                 <td>{props.issue.owner}</td>
                 <td>{props.issue.created.toDateString() }</td>
@@ -12,8 +12,8 @@ const IssueRow = (props) =>(
         </tr>
 )
 function IssueTable(props){
-        const issueRows = props.issues.map(issue =><IssueRow
-        key={issue.id} issue={issue} />);
+        const issueRows = props.issues.map(issue =><IssueRow 
+		key={issue._id} issue={issue} />)
         return(
                 <table className="bordered-table">
                         <thead>
@@ -75,20 +75,25 @@ class IssueList extends React.Component{
 	componentDidMount(){
 		this.loadData();
 	}
-	loadData(){
-		fetch('/api/issues').then(response　=>
-			response.json()
-		).then(data =>{
-			console.log("Total count of records:",data._metadata.total_count);
-			data.records.forEach(issue =>{
-				issue.created = new Date(issue.created);
-				if(issue.completionDate)
-					issue.completionDate = new Date(issue.completionDate);
-			});
-			this.setState({ issues: data.records });
-		}).catch(err =>{
-			console.log(err);
-		});
+	loadData() {
+ 	 fetch('/api/issues').then(response => {
+   		 if (response.ok) {
+     		 response.json().then(data => {
+       		 console.log("Total count of records:", data._metadata.total_count);
+        	data.records.forEach(issue => {
+          	issue.created = new Date(issue.created);
+          	if (issue.completionDate)
+            	issue.completionDate = new Date(issue.completionDate);
+        }); this.setState({ issues: data.records });
+ 	     });
+   	 } else {
+      	response.json().then(error => {
+        	alert("Failed to fetch issues:" + error.message)
+      	});
+    	}
+  	}).catch(err => {
+    	alert("Error in fetching data from server:", err);
+  	});
 	}
 	createIssue(newIssue){
 		fetch('/api/issues',{
